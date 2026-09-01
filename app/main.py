97123f590b66839236a79719095f6c2594b6dcad
Ravipati Tharaknadh <tharaknadhr19@gmail.com>
2026-09-01 19:49:40 +0530

    Configure async neon postgresql connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.database.database import AsyncSessionLocal
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "Smart task manager API is Running"}
+
+@app.get("/health")
+async def health_check():
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(text("SELECT 1"))
+        return {
+            "database":result.scalar()
+        }
